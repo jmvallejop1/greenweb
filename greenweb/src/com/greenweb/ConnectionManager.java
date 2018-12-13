f38fd7360e9186e0936ea8c02aa2c51ed3daaa96
Jmval <Jmval@DESKTOP-T3FR2E5>
2018-12-13 16:39:32 +0100

    conexion

--- greenweb/src/com/greenweb/ConnectionManager.java
+++ greenweb/src/com/greenweb/ConnectionManager.java
@@ -10,13 +10,21 @@ import java.util.TimeZone;
 public class ConnectionManager {
 	static Connection connection = null;
 	
-	public static Connection getConnection() throws Exception {
-		Class.forName("oracle.jdbc.driver.OracleDriver");
-	
-        // Setup the connection with the DB
-        connection = DriverManager.getConnection("jdbc:oracle:thin:@hendrix-oracle.cps.unizar.es:1521:vicious","a738435","zt3hh5");
-        return connection;
+	public static void main(String[] args) throws Exception {
+		getConnection();
 	}
 	
-
+	public static Connection getConnection() throws Exception {
+		//Class.forName("oracle.jdbc.driver.OracleDriver");
+		Class.forName("com.mysql.jdbc.Driver");
+		String url = "jdbc:mysql://" + "ec2-18-209-57-195.compute-1.amazonaws.com" + ":" + "3306" + "/" +"greenweb"+ "?user=" + "practicas_si" + "&password=" + "sisinfo" + "&useSSL=false";
+		//connection = DriverManager.getConnection("jdbc:mysql://ec2-18-209-57-195.compute-1.amazonaws.com:3306/greenweb","root","toor");
+		connection = DriverManager.getConnection(url);
+		//Connection con=DriverManager.getConnection(  
+		//		 "jdbc:mysql://localhost:3306/sonoo","root","root")
+        // Setup the connection with the DB
+        
+		//connection = DriverManager.getConnection("jdbc:oracle:thin:@hendrix-oracle.cps.unizar.es:1521:vicious","a738435","zt3hh5");
+        return connection;
+	}	
 }
